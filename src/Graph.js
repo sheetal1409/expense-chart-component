79315cda4bf3d8f117608703b31338fe907f2754
sheetal <sheetal.kulkarni91@gmail.com>
2023-03-30 17:42:21 -0700

    added chart js

--- src/Graph.js
+++ src/Graph.js
@@ -1,16 +1,69 @@
+
 import React from "react"
-// import { data } from "./data"
+import ReactDOM from "react-dom"
+import { Bar } from "react-chartjs-2"
+import { Chart as ChartJS } from "chart.js/auto"
+import { data } from "./data"
+
+
 
 export function Graph(props) {
 
+    const [userData, setUserData] = React.useState({
+        labels: props.data.map(data => data.day),
+        datasets: [{
+            label: "Spending - Last 7 Days",
+            fill: false,
+            data: props.data.map(data => data.amount),
+            backgroundColor: " hsl(10, 79%, 65%)",
+            border: 'none',
+            padding: '10',
+        }],
 
-    const items = props.data.map((item, idx) => <div className="graph-bar tooltip" style={{ "height": "span(item.amount)" }}>
-        <span className="tooltiptext">${item.amount}</span>
-        <div></div>
-        <h6 className="day">{item.day}</h6></div >)
+    })
+
+    const options = {
+        responsive: false,
+        scales: {
+            xAxes: [
+                {
+                    gridLines: {
+                        display: false,
+                        drawBorder: false,
+                        borderDash: [3, 3],
+                        zeroLineColor: "blue"
+                    },
+                    categoryPercentage: 0.7,
+                    barPercentage: 0.9,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ],
+            yAxes: [
+                {
+                    display: false,
+                    gridLines: {
+                        display: false,
+                        zeroLineColor: "transparent"
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    }
+
+    // const items = props.data.map((item, idx) => <div className="graph-bar tooltip" style={{ "height": "span(item.amount)" }}>
+    //     <span className="tooltiptext">${item.amount}</span>
+    //     <div>
+
+    //     </div>
+    //     <h6 className="day">{item.day}</h6></div >)
     return (
         <div className="graphdivs"  >
-            {items}
+            <div className="chartbar"> <Bar style={{ margin: 0, padding: 10 }} options={options} data={userData} /></div>
         </div>
     )
 }
